83bf56a36baf3698918720f57e07cdeaec3eeabe
Joey Copeland <josecope@iu.edu>
2019-11-18 23:31:19 -0500

    finishes the exercise.

---
+++ Scenes/FPScharacter.gd
@@ -0,0 +1,26 @@
+extends KinematicBody
+
+onready var camera = $Pivot/Camera
+
+var gravity = -30
+var max_speed = 8
+var mouse_sensitivity = 0.002 #radiansperpixel
+
+var velocity = Vector3()
+
+func get_input():
+	var input_dir = Vector3()
+	if Input.is_action_pressed("move_forward"):
+		input_dir += -camera.global_transform.basis.z
+	if Input.is_action_pressed("move_back"):
+		input_dir += -camera.global_transform.basis.z
+	if Input.is_action_pressed("strafe_left"):
+		input_dir += -camera.global_transform.basis.x
+	if Input.is_action_pressed("strafe_right"):
+		input_dir += -camera.global_transform.basis.x
+	input_dir = input_dir.normalized()
+	return input_dir
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Pivot.rotate.x(-event.relative.y * mouse_sensitivity)
